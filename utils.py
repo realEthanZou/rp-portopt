600d33114cc053322b23fc43a2894d89f9810429
Yifeng (Ethan) Zou <me@nekojita.moe>
2021-05-15 00:46:47 +0100

    Update get_trading_dates

--- utils.py
+++ utils.py
@@ -16,10 +16,10 @@ def get_raw_data(source, freq, verbose=True):
     :return: dataframe
     """
     if source == 'crsp':
-        if not Path(f'data/crsp_{freq}.h5').is_file():
+        if not Path(f"data/crsp_{freq}.h5").is_file():
             if verbose:
-                print(f'No cache found at data/crsp_{freq}.h5')
-                print(f'Loading from WRDS...')
+                print(f"No cache found at data/crsp_{freq}.h5")
+                print('Loading from WRDS...')
             db = wrds.Connection(wrds_username='realethanzou')
             df = db.raw_sql(
                 f"select permno, date, prc, vol, ret, shrout, cfacpr from crspq.{freq}sf where date between '{START_DATE}' and '{END_DATE}'")
@@ -37,7 +37,7 @@ def get_raw_data(source, freq, verbose=True):
 
             if verbose:
                 print(f"Saving cache to data/crsp_{freq}.h5 for key='raw'")
-            df.to_hdf(f'data/crsp_{freq}.h5', key='raw')
+            df.to_hdf(f"data/crsp_{freq}.h5", key='raw')
 
             return df
 
@@ -47,7 +47,7 @@ def get_raw_data(source, freq, verbose=True):
             return pd.read_hdf(f'data/crsp_{freq}.h5', key='raw')
 
     elif source == 'ff':
-        if not Path(f'data/ff_{freq}.h5').is_file():
+        if not Path(f"data/ff_{freq}.h5").is_file():
             if freq == 'm':
                 label = 'monthly'
             elif freq == 'd':
@@ -56,8 +56,8 @@ def get_raw_data(source, freq, verbose=True):
                 raise ValueError
 
             if verbose:
-                print(f'No cache found at data/ff_{freq}.h5')
-                print(f'Loading from WRDS...')
+                print(f"No cache found at data/ff_{freq}.h5")
+                print('Loading from WRDS...')
             db = wrds.Connection(wrds_username='realethanzou')
             df = db.raw_sql(
                 f"select date, mktrf, smb, hml, rf, umd from ff.factors_{label} where date between '{START_DATE}' and '{END_DATE}'")
@@ -70,14 +70,14 @@ def get_raw_data(source, freq, verbose=True):
 
             if verbose:
                 print(f"Saving cache to data/ff_{freq}.h5 for key='raw'")
-            df.to_hdf(f'data/ff_{freq}.h5', key='raw')
+            df.to_hdf(f"data/ff_{freq}.h5", key='raw')
 
             return df
 
         else:
             if verbose:
                 print(f"Loading cache from data/ff_{freq}.h5 for key='raw'")
-            return pd.read_hdf(f'data/ff_{freq}.h5', key='raw')
+            return pd.read_hdf(f"data/ff_{freq}.h5", key='raw')
 
     else:
         raise ValueError
@@ -93,25 +93,25 @@ def get_data(source, freq, key='raw', verbose=True):
     :return: dataframe
     """
     if source == 'crsp':
-        if not Path(f'data/crsp_{freq}.h5').is_file():
+        if not Path(f"data/crsp_{freq}.h5").is_file():
             get_raw_data(source, freq)
 
         try:
             if verbose:
                 print(f"Loading cache from data/crsp_{freq}.h5 for key='{key}'")
-            return pd.read_hdf(f'data/crsp_{freq}.h5', key=key)
+            return pd.read_hdf(f"data/crsp_{freq}.h5", key=key)
 
         except KeyError:
             if verbose:
                 print(f"No cache found at data/crsp_{freq}.h5 for key='{key}'")
 
             if key in ['ret', 'acprc', 'dolvol', 'cap']:
-                df = pd.read_hdf(f'data/crsp_{freq}.h5', 'raw')[['permno', 'date', key]]
+                df = pd.read_hdf(f"data/crsp_{freq}.h5", 'raw')[['permno', 'date', key]]
                 df = df.pivot('date', 'permno', key)
 
                 if verbose:
                     print(f"Saving cache to data/crsp_{freq}.h5 for key='{key}'")
-                df.to_hdf(f'data/crsp_{freq}.h5', key=key)
+                df.to_hdf(f"data/crsp_{freq}.h5", key=key)
 
                 return df
 
@@ -119,12 +119,12 @@ def get_data(source, freq, key='raw', verbose=True):
                 raise ValueError
 
     elif source == 'ff':
-        if not Path(f'data/ff_{freq}.h5').is_file():
+        if not Path(f"data/ff_{freq}.h5").is_file():
             get_raw_data(source, freq)
 
         if verbose:
             print(f"Loading cache from data/ff_{freq}.h5 for key='{key}'")
-        return pd.read_hdf(f'data/ff_{freq}.h5', key=key)
+        return pd.read_hdf(f"data/ff_{freq}.h5", key=key)
 
     else:
         raise ValueError
@@ -161,34 +161,33 @@ def get_sampled_subset(df_ret, n):
     return df_ret.sample(n, random_state=42, axis=1).sort_index(axis=1)
 
 
-def get_trading_dates(by='all', year=None, month=None):
-    """
-    Helper funtion to get trading dates
-    :param by: 'all' or 'first' or 'last' trading date(s) of filtered month(s)
-    :param year: filter certain year
-    :param month: filter certain month
-    :return: list of trading date string(s)
-    """
-    if not Path(f'data/cal.csv').is_file():
+def get_trading_dates(by='all', year=None, month=None, day=None, before=0, after=0):
+    if not Path(f"data/cal.csv").is_file():
         cal = pd.DataFrame({'full': get_data('ff', 'd', verbose=False).date})
+        cal['year'] = cal.full.apply(lambda x: int(x[:4]))
+        cal['month'] = cal.full.apply(lambda x: int(x[5:7]))
+        cal['day'] = cal.full.apply(lambda x: int(x[8:]))
         cal.to_csv('data/cal.csv')
 
     else:
-        cal = pd.read_csv(f'data/cal.csv', index_col=0)
+        cal = pd.read_csv(f"data/cal.csv", index_col=0)
 
     if year is not None:
+        assert isinstance(year, (int, str))
         year = int(year)
-        assert int(START_DATE[:4]) <= year <= int(END_DATE[:4])
-        year = str(year)
+        assert int(START_DATE[:4]) <= int(year) <= int(END_DATE[:4])
 
     if month is not None:
+        assert isinstance(month, (int, str))
         month = int(month)
         assert 1 <= month <= 12
-        month = f'{month:02}'
 
-    cal['year'] = cal.full.apply(lambda x: x[:4])
-    cal['month'] = cal.full.apply(lambda x: x[5:7])
-    cal['date'] = cal.full.apply(lambda x: x[8:])
+    if day is not None:
+        assert isinstance(day, (int, str)) and year is not None and month is not None
+        day = int(day)
+        assert len(cal.query("year == @year and month == @month and day == @day")) == 1
+
+    assert before >= 0 and after >= 0
 
     if by == 'all':
         pass
@@ -200,13 +199,60 @@ def get_trading_dates(by='all', year=None, month=None):
         raise ValueError
 
     if year is None and month is None:
+        assert before == 0 and after == 0
+
         return cal.full.to_list()
 
     elif year is not None and month is None:
-        return cal.query("year == @year").full.to_list()
+        if before == 0 and after == 0:
+            return cal.query("year == @year").full.to_list()
+
+        else:
+            return cal.query("@year - @before + 1 <= year <= @year").full.to_list(), \
+                   cal.query("@year < year <= @year + @after").full.to_list()
 
     elif year is None and month is not None:
+        assert before == 0 and after == 0
+
         return cal.query("month == @month").full.to_list()
 
     else:
-        return cal.query("year == @year and month == @month").full.to_list()
+        if day is None:
+            if before == 0 and after == 0:
+                return cal.query("year == @year and month == @month").full.to_list()
+
+            else:
+                assert by == 'all'
+                cal_orig = cal.copy()
+                cal = cal.groupby(['year', 'month']).max().reset_index()
+                base_idx = cal.query("year == @year and month == @month").index[0]
+                from_idx = base_idx - before + 1
+                to_idx = base_idx + after
+                assert from_idx >= 0 and to_idx <= len(cal)
+                from_year = cal.iloc[from_idx].year
+                from_month = cal.iloc[from_idx].month
+                to_year = cal.iloc[to_idx].year
+                to_month = cal.iloc[to_idx].month
+                from_idx = cal_orig.query("year == @from_year and month == @from_month").index[0]
+                to_indices = cal_orig.query("year == @to_year and month == @to_month").index
+
+                if after == 0:
+                    base_idx, to_idx = to_indices[-1], to_indices[-1]
+                else:
+                    base_idx, to_idx = to_indices[0] - 1, to_indices[-1]
+
+                return cal_orig[from_idx: base_idx + 1].full.to_list(), \
+                       cal_orig[base_idx + 1: to_idx + 1].full.to_list()
+
+        else:
+            if before == 0 and after == 0:
+                return cal.query("year == @year and month == @month and day == @day").full.to_list()
+
+            else:
+                assert by == 'all'
+                base_idx = cal.query("year == @year and month == @month and day == @day").index[0]
+                from_idx = base_idx - before + 1
+                to_idx = base_idx + after
+                assert from_idx >= 0 and to_idx <= len(cal)
+
+                return cal[from_idx: base_idx + 1].full.to_list(), cal[base_idx + 1: to_idx + 1].full.to_list()
