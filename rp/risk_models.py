cb3fdeffd9a6bd6bd3da18595d42e0dc86cad402
Yifeng (Ethan) Zou <me@nekojita.moe>
2021-05-20 00:00:27 +0100

    Add some linear shrinkage models

--- rp/risk_models.py
+++ rp/risk_models.py
@@ -1,41 +1,138 @@
-import warnings
-
 import numpy as np
 import pandas as pd
 
-
-def _is_positive_semidefinite(df_cov):
-    try:
-        np.linalg.cholesky(df_cov + 1e-16 * np.eye(len(df_cov)))
-        return True
-    except np.linalg.LinAlgError:
-        return False
+from .utils import fix_non_psd
 
 
-def fix_nonpositive_semidefinite(df_cov):
-    if _is_positive_semidefinite(df_cov):
-        return df_cov
+def get_risk_matrix(df_ret, method):
+    if method == 'sample':
+        return sample_covariance(df_ret)
 
-    warnings.warn("The covariance matrix is non positive semidefinite. Amending eigenvalues.")
-    permnos = df_cov.index
+    elif method == 'ls_scaled_identity':
+        return linear_shrinkage(df_ret, target='scaled_identity')
 
-    q, V = np.linalg.eigh(df_cov)
-    q = np.where(q > 0, q, 0)
-    fixed_matrix = V @ np.diag(q) @ V.T
+    elif method == 'ls_single_factor':
+        return linear_shrinkage(df_ret, target='single_factor')
 
-    if not _is_positive_semidefinite(fixed_matrix):
-        warnings.warn('Could not fix matrix. Please try a different risk model.')
-
-    return pd.DataFrame(fixed_matrix, index=permnos, columns=permnos)
-
-
-def sample_cov(df_ret, freq=250):
-    return fix_nonpositive_semidefinite(df_ret.cov() * freq)
-
-
-def get_risk_matrix(df_ret, method, **kwargs):
-    if method == 'sample_cov':
-        return sample_cov(df_ret, **kwargs)
+    elif method == 'ls_constant_corr':
+        return linear_shrinkage(df_ret, target='constant_corr')
 
     else:
-        return ValueError
+        raise ValueError
+
+
+def sample_covariance(df_ret):
+    return fix_non_psd(df_ret.cov(), df_ret.index[-1])
+
+
+def linear_shrinkage(df_ret, target):
+    if target == 'scaled_identity':
+        return _linear_shrinkage_constant_corr(df_ret)
+
+    elif target == 'single_factor':
+        return _linear_shrinkage_single_factor(df_ret)
+
+    elif target == 'constant_corr':
+        return _linear_shrinkage_constant_corr(df_ret)
+
+    else:
+        raise ValueError
+
+
+def _linear_shrinkage_scaled_identity(df_ret):
+    # Ledoit, O. and Wolf, M. (2004).
+    # A well-conditioned estimator for large-dimensional covariance matrices.
+    # Journal of Multivariate Analysis, 88:365-411.
+    x = np.nan_to_num(df_ret.values)
+    t, n = np.shape(x)
+    x = x - x.mean(axis=0)
+
+    sample = np.cov(x, ddof=0, rowvar=False)
+    target = np.mean(np.diag(sample)) * np.eye(n)
+
+    y = x ** 2
+    p_arr = y.T.dot(y) / t - sample ** 2
+    p = np.sum(p_arr)
+    c = np.linalg.norm(sample - target, "fro") ** 2
+
+    k = p / c
+    delta = max(0., min(1., k / t))
+    shrunk_cov = delta * target + (1 - delta) * sample
+
+    return fix_non_psd(pd.DataFrame(shrunk_cov, index=df_ret.columns, columns=df_ret.columns), df_ret.index[-1]), delta
+
+
+def _linear_shrinkage_single_factor(df_ret):
+    # Ledoit, O. and Wolf, M. (2003).
+    # Improved estimation of the covariance matrix of stock returns with an application to portfolio selection.
+    # Journal of Empirical Finance, 10:603-621.
+    x = np.nan_to_num(df_ret.values)
+    t, n = np.shape(x)
+    x = x - x.mean(axis=0)
+    xmkt = np.mean(x, axis=1).reshape(t, 1)
+
+    sample = np.cov(np.append(x, xmkt, axis=1), ddof=0, rowvar=False)
+    covmkt = sample[:n, n].reshape(n, 1)
+    varmkt = sample[n, n]
+    sample = sample[:n, :n]
+    target = covmkt.dot(covmkt.T) / varmkt
+    target[np.eye(n) == 1] = np.diag(sample)
+
+    y = x ** 2
+    p_arr = y.T.dot(y) / t - sample ** 2
+    p = np.sum(p_arr)
+    c = np.linalg.norm(sample - target, "fro") ** 2
+
+    rdiag = np.sum(y ** 2 / t) - np.sum(np.diag(sample) ** 2)
+    z = x * np.tile(xmkt, n)
+    v1 = 1 / t * y.T.dot(z) - np.tile(covmkt, n) * sample
+    roff1 = (np.sum(v1 * np.tile(covmkt, n).T) - np.sum(np.diag(v1) * covmkt.T)) / varmkt
+    v3 = 1 / t * z.T.dot(z) - varmkt * sample
+    roff3 = (np.sum(v3 * covmkt.dot(covmkt.T)) - np.sum(np.diag(v3).reshape(n, 1) * covmkt ** 2)) / varmkt ** 2
+    roff = 2 * roff1 - roff3
+    r = rdiag + roff
+
+    k = (p - r) / c
+    delta = max(0., min(1., k / t))
+    shrunk_cov = delta * target + (1 - delta) * sample
+
+    return fix_non_psd(pd.DataFrame(shrunk_cov, index=df_ret.columns, columns=df_ret.columns), df_ret.index[-1]), delta
+
+
+def _linear_shrinkage_constant_corr(df_ret):
+    # Ledoit, O. and Wolf, M. (2004).
+    # Honey, I shrunk the sample covariance matrix.
+    # Journal of Portfolio Management, 30(4):110-119.
+    x = np.nan_to_num(df_ret.values)
+    t, n = np.shape(x)
+    x = x - x.mean(axis=0)
+
+    sample = np.cov(x, ddof=0, rowvar=False)
+    var = np.diag(sample).reshape(n, 1)
+    std = np.sqrt(var)
+    var_arr = np.tile(var, n)
+    std_arr = np.tile(std, n)
+    r_bar = (np.sum(sample / (std_arr * std_arr.T)) - n) / (n * (n - 1))
+    target = r_bar * std_arr * std_arr.T
+    target[np.eye(n) == 1] = var.reshape(n)
+
+    y = x ** 2
+    p_arr = y.T.dot(y) / t - sample ** 2
+    p = np.sum(p_arr)
+    c = np.linalg.norm(sample - target, "fro") ** 2
+
+    term1 = (x ** 3).T.dot(x) / t
+    help_ = x.T.dot(x) / t
+    help_diag = np.diag(help_).reshape(n, 1)
+    term2 = np.tile(help_diag, n).T * sample
+    term3 = help_ * var_arr
+    term4 = var_arr * sample
+    theta_arr = term1 - term2 - term3 + term4
+    theta_arr[np.eye(n) == 1] = np.zeros(n)
+    r = sum(np.diag(p_arr)) + r_bar * np.sum((1. / std).dot(std.T) * theta_arr)
+
+    k = (p - r) / c
+    delta = max(0., min(1., k / t))
+    shrunk_cov = delta * target + (1 - delta) * sample
+
+    return fix_non_psd(pd.DataFrame(shrunk_cov, index=df_ret.columns, columns=df_ret.columns), df_ret.index[-1]), delta
