b71abb79ad83e811ed1bc4a4f4bd05cb6e8a098b
Yifeng (Ethan) Zou <me@nekojita.moe>
2021-05-24 18:02:35 +0100

    Add variance and sharpe ratio

--- rp/backtest.py
+++ rp/backtest.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from .utils import gen_crsp_subset, BACKTEST_START_YEAR, BACKTEST_START_MONTH
 from .weights import get_weights
 
 
@@ -5,18 +8,35 @@ def run_backtest(codename, lookback=120, holding=1, freq='m', n_sample=500, seed
     df_weights = get_weights(codename=codename, lookback=lookback, holding=holding, freq=freq, n_sample=n_sample,
                              seed=seed, verbose=verbose)
 
+    returns = _calc_returns(df_weights, lookback=lookback, holding=holding, freq=freq, n_sample=n_sample, seed=seed)
+    variance = np.var(returns)
+    sharpe = np.mean(returns) / np.std(returns)
     turnover = calc_turnover(df_weights)
-    print(f"turnover: {turnover:f}")
+    print(f"variance: {variance:f}, sharpe: {sharpe:f}, turnover: {turnover:f}")
 
-    return df_weights
-
-
-def calc_variance():
-    pass
+    return
 
 
-def calc_sharpe():
-    pass
+def _calc_returns(df_weights, lookback, holding, freq, n_sample, seed):
+    if freq == 'm':
+        day = 'last'
+    elif freq == 'd':
+        day = 30
+    else:
+        raise ValueError
+
+    ret_gen = gen_crsp_subset('ret', year=BACKTEST_START_YEAR, month=BACKTEST_START_MONTH, day=day, before=lookback,
+                              after=holding, rolling_freq=holding, n_sample=n_sample, seed=seed)
+
+    idx = 0
+    returns = []
+    for _, df_after in ret_gen:
+        df_return = (df_after.fillna(0) + 1).prod() - 1
+        ret = (df_weights.iloc[idx][df_return.index] * df_return).sum()
+        returns.append(ret)
+        idx += 1
+
+    return returns
 
 
 def calc_turnover(df_weights):
